5988cf2f9364f271e41688c2c70a341235dd5875
Mathias Jacquart <math.jacquart@gmail.com>
2024-06-18 17:32:26 +0200

    admin dashboard creation

--- FRONT-END/src/components/pages/admin/AdminDashboard.jsx
+++ FRONT-END/src/components/pages/admin/AdminDashboard.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { Admin, Resource } from 'react-admin';
+import simpleRestProvider from 'ra-data-simple-rest';
+import { ItemList, ItemEdit, ItemCreate } from './components/ItemComponents';
+
+const dataProvider = simpleRestProvider('http://localhost:4560/api');
 
 export default function AdminDashboard() {
   return (
-    <div>AdminDashboard</div>
+    <Admin dataProvider={dataProvider}>
+    <Resource name="items" list={ItemList} edit={ItemEdit} create={ItemCreate} />
+  </Admin>
   )
 }
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,15 +1,25 @@
+// routes/users.js
+const express = require("express");
 const {
     signupUser,
     verifyMail,
     signinUser,
-  } = require("../controllers/users-controller");
-  
-  const router = require("express").Router();
-  
-  router.post("/signup", signupUser);
-  
-  router.post("/signin", signinUser);
-  
-  router.get("/verifyMail/:token", verifyMail);
-  
-  module.exports = router;
+    getUsers,
+    getUser,
+    updateUser,
+    deleteUser
+} = require("../controllers/users-controller");
+
+const router = express.Router();
+
+router.post("/signup", signupUser);
+router.post("/signin", signinUser);
+router.get("/verifyMail/:token", verifyMail);
+
+// CRUD operations for react-admin
+router.get("/", getUsers); // GET all users
+router.get("/:id", getUser); // GET single user by ID
+router.put("/:id", updateUser); // UPDATE user by ID
+router.delete("/:id", deleteUser); // DELETE user by ID
+
+module.exports = router;
