fd777d01d3e22fd075e2fa46e84c869533d9948a
mathiasjacquart <math.jacquart@gmail.com>
2025-05-05 16:32:14 +0200

    fix: configuration des chemins et types MIME

--- FRONT-END/vite.config.js
+++ FRONT-END/vite.config.js
@@ -1,29 +1,32 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   build: {
     outDir: "dist",
+    assetsDir: "assets",
     sourcemap: true,
     rollupOptions: {
       output: {
-        manualChunks: undefined,
+        manualChunks: {
+          vendor: ["react", "react-dom", "react-router-dom"],
+        },
       },
     },
   },
   server: {
     port: 3000,
-    host: true,
+    open: true,
   },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "./",
   optimizeDeps: {
     include: ["react", "react-dom", "react-router-dom"],
   },
