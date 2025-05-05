2b72adfd50ec740ec3af82608df2c91267f62562
mathiasjacquart <math.jacquart@gmail.com>
2025-05-05 13:31:09 +0200

    vercel modif

--- FRONT-END/vite.config.js
+++ FRONT-END/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,4 +18,10 @@ export default defineConfig({
     port: 3000,
     host: true,
   },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  base: "/",
 });
