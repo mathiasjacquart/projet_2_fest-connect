4b9462a41300fd3f10539eeb570ef65188c2c1ae
mathiasjacquart <math.jacquart@gmail.com>
2025-05-05 12:29:19 +0200

    front vercel

--- FRONT-END/vite.config.js
+++ FRONT-END/vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist",
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  server: {
+    port: 3000,
+    host: true,
+  },
+});
