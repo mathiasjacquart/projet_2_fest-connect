09f2e594338d7347da54bc2cbef507c2f9a39dc1
Mathias JACQUART <150044178+mathiasjacquart@users.noreply.github.com>
2025-05-13 11:03:04 +0200

    img solve

--- FRONT-END/vite.config.js
+++ FRONT-END/vite.config.js
@@ -5,16 +5,29 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  // Changement important : utilisez '/' au lieu de './'
+  base: "/",
   build: {
     outDir: "dist",
     assetsDir: "assets",
     sourcemap: true,
     rollupOptions: {
       output: {
-        entryFileNames: "assets/[name].[hash].js",
-        chunkFileNames: "assets/[name].[hash].js",
-        assetFileNames: "assets/[name].[hash].[ext]",
+        // Configuration simplifiée mais efficace
+        entryFileNames: "assets/js/[name].[hash].js",
+        chunkFileNames: "assets/js/[name].[hash].js",
+        assetFileNames: (assetInfo) => {
+          // Garde la structure de dossiers pour les images
+          if (/\.(png|jpe?g|gif|svg|webp|ico)$/i.test(assetInfo.name)) {
+            return `assets/img/[name].[hash].[ext]`;
+          }
+
+          if (/\.(css|scss|sass)$/i.test(assetInfo.name)) {
+            return `assets/css/[name].[hash].[ext]`;
+          }
+
+          return `assets/[name].[hash].[ext]`;
+        },
         manualChunks: {
           vendor: ["react", "react-dom", "react-router-dom"],
         },
@@ -25,8 +38,5 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  optimizeDeps: {
-    include: ["react", "react-dom", "react-router-dom"],
   },
 });
